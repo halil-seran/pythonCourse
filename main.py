53f89f09fce364540076a188dd96014351e9400d
halil-seran <halil.seran.1135@gmail.com>
2023-12-12 15:41:57 +0300

    new

--- main.py
+++ main.py
@@ -8,8 +8,49 @@
 #full_name = first_name + ' ' + last_name
 #print(full_name)
 
-age = 25
+#age = 25
 #age += 1
-print("Your age: " + str(age))
+#print("Your age: " + str(age))
 
-height = 250.5
+#height = 250.5
+#print(type(height))
+
+#isHuman = True
+#print(isHuman)
+
+#name, age, isHuman = 'halil', 25, True
+#print(name, age, isHuman)
+
+#age1 = age2 = age3 = 25
+#print(age1, age2, age3)
+
+# name = 'halil'
+#
+# print(name)
+# print(len(name))
+# print(name.find('i'))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count('l'))
+# print(name.replace('l', 'm'))
+# print(name*3)
+
+# x=1
+# y=2.1
+# z='3'
+#
+# x=float(x)
+# y=str(y)
+# z=int(z)
+#
+# print(x)
+# print(y)
+# print(z*3)
+
+
+
+
+
