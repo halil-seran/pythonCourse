326ac3fe7717e66e18f4ae05d3f7b60d60a09b0a
halil-seran <halil.seran.1135@gmail.com>
2023-12-12 15:05:52 +0300

    new

---
+++ main.py
@@ -0,0 +1,14 @@
+# print('lets try again')
+
+#name = ('halil')
+#print('hello', name)
+# print(type(name))
+#first_name = 'halil'
+#last_name = 'seran'
+#full_name = first_name + ' ' + last_name
+#print(full_name)
+
+age = 25
+#age += 1
+print("Your age: " + str(age))
+
