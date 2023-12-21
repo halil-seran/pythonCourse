e3795f02e5ce8f0056a9f363fdd4dac23e7097d6
halil-seran <halil.seran.1135@gmail.com>
2023-12-21 17:17:56 +0300

    Half way

--- main.py
+++ main.py
@@ -1183,7 +1183,35 @@
 # answer = input("Do you wish to exit? ")
 
 
+from multiprocessing import Process, cpu_count
+import time
 
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+def main():
+    a = Process(target=counter, args=(100000000,))
+    b = Process(target=counter, args=(100000000,))
+    c = Process(target=counter, args=(100000000,))
+    d = Process(target=counter, args=(100000000,))
+
+    a.start()
+    b.start()
+    c.start()
+    d.start()
+
+    a.join()
+    b.join()
+    c.join()
+    d.join()
+
+    print("Finished in: ", time.perf_counter(), "seconds")
+
+if __name__ == '__main__':
+    main()
 
 
 
