e4ffc84ef373100ee0ce3f3a1d6c8ccea4216f40
halil-seran <halil.seran.1135@gmail.com>
2023-12-25 10:47:53 +0300

    game

---
+++ pig-Game.py
@@ -0,0 +1,50 @@
+import random
+
+def rollDice():
+    return random.randint(1,6)
+
+def main():
+    player1 = 0
+    player2 = 0
+    turn = 0
+    while player1 < 100 and player2 < 100:
+        if turn == 0:
+            print("Player 1's turn")
+            print("Current score of player 1: ", player1)
+            print("Current score of player 2: ", player2)
+            print("Rolling dice...")
+            dice = rollDice()
+            print("Dice value: ", dice)
+            if dice == 1:
+                print("You rolled a 1. Your turn is over.")
+                turn = 1
+            else:
+                player1 += dice
+                print("Your score is now: ", player1)
+                print("Would you like to roll again? (y/n)")
+                choice = input()
+                if choice == 'n':
+                    turn = 1
+        else:
+            print("Player 2's turn")
+            print("Current score of player 1: ", player1)
+            print("Current score of player 2: ", player2)
+            print("Rolling dice...")
+            dice = rollDice()
+            print("Dice value: ", dice)
+            if dice == 1:
+                print("You rolled a 1. Your turn is over.")
+                turn = 0
+            else:
+                player2 += dice
+                print("Your score is now: ", player2)
+                print("Would you like to roll again? (y/n)")
+                choice = input()
+                if choice == 'n':
+                    turn = 0
+    if player1 >= 100:
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+
+main()
